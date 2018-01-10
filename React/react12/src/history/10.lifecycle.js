486d8acea202231de34686b78f28c60bf47a32f0
zjqzlyzjq <2193542487@qq.com>
2018-01-10 18:02:52 +0800

    生命周期

--- React/react12/src/history/10.lifecycle.js
+++ React/react12/src/history/10.lifecycle.js
@@ -1,78 +1,69 @@
-import React, {Component} from 'react'
-import ReactDom,{render} from 'react-dom';
+import React,{Component} from 'react'
+import ReactDOM,{render} from 'react-dom';
 
-class Counter extends React.Component {//React.PureComponent 它会比较两个状态相等就不会刷新视图 浅比较
+//defaultProps-> constructor
+class Counter extends React.Component{ // 他会比较两个状态相等就不会刷新视图 PureComponent是浅比较
     static defaultProps = {
-        name: 'zzz'
+        name:'珠峰培训'
     };
-
-    constructor(props) {
+    constructor(props){
         super();
-        this.state = {number: 0};
-        console.log('1. constructor');
+        this.state = {number:0};
+        console.log('1.constructor构造函数')
     }
-
-    componentWillMount() {/// 取本地的数据 同步的方式：采用渲染之前获取数据，只渲染一次
-        console.log('2.组件将要加载   componentWillMount');
+    componentWillMount(){ // 取本地的数据 同步的方式：采用渲染之前获取数据，只渲染一次
+        console.log('2.组件将要加载 componentWillMount');
     }
-
-    componentDidMount() {
-        console.log('4.组件挂载完成  componentDidMount');
+    componentDidMount(){
+        console.log('4.组件挂载完成 componentDidMount');
     }
-
-    handleClick = () => {
-        this.setState({number: this.state.number + 1})
+    handleClick=()=>{
+        this.setState({number:this.state.number+1});
     };
-    /* 不要随便使用setState 可能会死循环   */
-
-    //
-    shouldComponentUpdate(nextProps, nextState) {
-        console.log('5.组件是否更新    shouldComponentUpdate');
-        return nextState.number % 2; //如果此函数种返回了false 就不会调用render方法了
+    // react可以shouldComponentUpdate方法中优化 PureComponent 可以帮我们做这件事
+    shouldComponentUpdate(nextProps,nextState){ // 代表的是下一次的属性 和 下一次的状态
+        console.log('5.组件是否更新 shouldComponentUpdate');
+        return nextState.number%2;
+        // return nextState.number!==this.state.number; //如果此函数种返回了false 就不会调用render方法了
+    } //不要随便用setState 可能会死循环
+    componentWillUpdate(){
+        console.log('6.组件将要更新 componentWillUpdate');
     }
-
-    componentWillUpdate() {
-        console.log('6.组件将要更新   componentWillUpdate');
-    }
-
-    componentDidUpdate() {
+    componentDidUpdate(){
         console.log('7.组件完成更新 componentDidUpdate');
     }
-    render() {
+    render(){
         console.log('3.render');
         return (
             <div>
                 <p>{this.state.number}</p>
-                {this.state.number>3?null:<ChildCounter n={this.state.name}/>}
+                {this.state.number>3?null:<ChildCounter n={this.state.number}/>}
                 <button onClick={this.handleClick}>+</button>
             </div>
         )
     }
 }
-class ChildCounter extends Component {
+class ChildCounter extends Component{
     componentWillUnmount(){
-        console.log(' componentWillUnmount 组件将要卸载');
+        console.log('组件将要卸载componentWillUnmount')
     }
     componentWillMount(){
         console.log('child componentWillMount')
     }
-    render() {
+    render(){
         console.log('child-render');
-        return (
-
-            <div>
-                {this.props.n}
-            </div>
-        )
+        return (<div>
+            {this.props.n}
+        </div>)
     }
-    componentDidMount() {
-        console.log('chlid DidMount');
+    componentDidMount(){
+        console.log('child componentDidMount')
     }
-    componentWillReceiveProps(newProps) { // 第一次不会执行，之后属性更新时才会执行
+    componentWillReceiveProps(newProps){ // 第一次不会执行，之后属性更新时才会执行
         console.log('child componentWillReceiveProps')
     }
-    shouldComponentUpdate(nextProps, nextState) {
-        return nextProps.n % 3; //子组件判断接收的属性 是否满足更新条件 为true则更新
+    shouldComponentUpdate(nextProps,nextState){
+        return nextProps.n%3; //子组件判断接收的属性 是否满足更新条件 为true则更新
     }
 }
-ReactDom.render(<Counter name="计数器"/>, window.root);
+ReactDOM.render(<Counter name="计数器"/>,window.root);
